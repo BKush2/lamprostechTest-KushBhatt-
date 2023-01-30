835ac367d6d7157d8955e540a2c37f1980387e9b
BKush2 <123766783+BKush2@users.noreply.github.com>
2023-01-30 17:40:44 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String args[])
+	{
+	    int =3,m=3;
+	 public int[] twoSum(int[] nums, int []nums 2,) 
+	 
+    for (int i = 0; i < nums.length; i++)
+    {
+        for (int j = 0; j < nums.length; j++) 
+        {
+            if (nums[i]+num(j)==target){
+                return new int[] { i, j };
+            }
+          
+        }
+    }
+	}
+	}
+}
+    
+
